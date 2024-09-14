f9c3120ba51b76b6534ed6713b833f76360e0aef
GOWSHIK <gowshikofficial2022@gmail.com>
2024-09-14 12:01:21 +0530

    mul() added

--- src/main/java/calcpkg/MyCalc.java
+++ src/main/java/calcpkg/MyCalc.java
@@ -11,10 +11,17 @@ public class MyCalc {
 	public int difference(int a , int b) {
 		return a-b;
 	}
+	
+	public int mul(int a , int b) {
+		return a&b;
+	}
+	
 	public static void main(String[] args) {
 		MyCalc ob = new MyCalc();
 		System.out.println("Sum is : " + ob.sum(4, 5));
 		System.out.println("Difference is " + ob.difference(5, 5));
+		System.out.println("Multiplication is : " + ob.mul(4, 5));
+
 	}
 
 }
