04d3bf715bb38c4e82aaf2d87df52d7eda971780
GOWSHIK <gowshikofficial2022@gmail.com>
2024-09-14 12:16:20 +0530

    added div()

--- src/main/java/calcpkg/MyCalc.java
+++ src/main/java/calcpkg/MyCalc.java
@@ -15,12 +15,16 @@ public class MyCalc {
 	public int mul(int a , int b) {
 		return a&b;
 	}
+	public int div(int a , int b) {
+		return a/b;
+	}
 	
 	public static void main(String[] args) {
 		MyCalc ob = new MyCalc();
 		System.out.println("Sum is : " + ob.sum(4, 5));
 		System.out.println("Difference is " + ob.difference(5, 5));
 		System.out.println("Multiplication is : " + ob.mul(4, 5));
+		System.out.println("Division  is : " + ob.div(5, 2));
 
 	}
 
